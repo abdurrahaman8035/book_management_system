161b09225bd6be7e0810625142f5337897f77d96
abdurrahaman8035 <abdurrahamanabdullahi20@gmail.com>
2023-06-25 17:49:53 +0100

    Resolved migrations conflict

--- books/migrations/0007_auto_20230625_1731.py
+++ books/migrations/0007_auto_20230625_1731.py
@@ -12,21 +12,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='StudentBook',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100, verbose_name='Book title')),
-                ('issued_date', models.DateTimeField(default=datetime.datetime(2023, 6, 25, 17, 31, 47, 218469))),
-                ('added_days', models.IntegerField(blank=True, null=True)),
-                ('remaining_days', models.CharField(blank=True, max_length=100, null=True)),
-                ('expiring_date', models.DateTimeField(blank=True, default=datetime.datetime(2023, 7, 9, 17, 31, 47, 218577), null=True)),
-                ('borrowed_by', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='books', to='books.Student')),
-            ],
-            options={
-                'ordering': ['-issued_date'],
-            },
-        ),
         migrations.AlterField(
             model_name='staffbook',
             name='expiring_date',
