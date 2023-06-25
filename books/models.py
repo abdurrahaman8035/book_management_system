c2d0276b83cd84bb4d74b6a554a3c2307a1b7b57
abdurrahaman8035 <abdurrahamanabdullahi20@gmail.com>
2023-06-25 07:38:26 +0100

    Refactored models to enhance code readability and better performance

--- books/models.py
+++ books/models.py
@@ -1,20 +1,9 @@
+from datetime import datetime, timedelta
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db import models
 from django.urls import reverse
-
-# from datetime import *
 from django.core.mail import EmailMessage
-
-
-class Year_Of_Reg(models.Model):
-    year = models.CharField(max_length=9)
-
-    class Meta:
-        verbose_name_plural = "years of admission"
-
-    def __str__(self):
-        return f"{self.year}".title()
 
 
 LEVEL_CHOICES = [
@@ -26,111 +15,134 @@ LEVEL_CHOICES = [
 ]
 
 
-class Student(models.Model):
-    """Database table for storing information
-    about a particular student that borrowed a book"""
+class YearOfAdmission(models.Model):
+    """Year of entry for Students"""
+
+    year = models.CharField(max_length=9)
 
     class Meta:
-        ordering = ["-registration_date"]
-        verbose_name_plural = "students"
+        """Additional information for the Class"""
+
+        verbose_name_plural = "years of admission"
+
+    def __str__(self):
+        return f"{self.year}".title()
+
+
+class UserAbstractModel(models.Model):
+    """Abstract Class for Student and Staff Classes"""
+
+    class Meta:
+        """Additional infromation for the Class"""
+
+        abstract = True
 
     image = models.ImageField(upload_to="images//%Y/%m/%d/", blank=False, null=True)
     first_name = models.CharField(max_length=100)
     second_name = models.CharField(max_length=100)
-    id_number = models.CharField(max_length=15, unique=True)
     Email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
     registration_date = models.DateTimeField(auto_now_add=True)
+    email_sent = models.BooleanField(null=True, blank=True, default=False)
+    sms_sent = models.BooleanField(null=True, blank=True, default=False)
+
+
+class Student(UserAbstractModel, models.Model):
+    """Database table for students information"""
+
+    class Meta:
+        """Additional information for the Student Class"""
+
+        ordering = ["-registration_date"]
+        verbose_name_plural = "students"
+
+    student_id = models.CharField(max_length=15, unique=True)
     year_of_admission = models.ForeignKey(
-        Year_Of_Reg, on_delete=models.CASCADE, null=True, blank=True
+        YearOfAdmission, on_delete=models.CASCADE, null=True, blank=True
     )
-    image_url = models.CharField(max_length=100, null=True, blank=True)
     level = models.CharField(
         max_length=10,
         choices=LEVEL_CHOICES,
     )
-    email_sent = models.BooleanField(null=True, blank=True, default=False)
-    sms_sent = models.BooleanField(null=True, blank=True, default=False)
 
     def get_absolute_url(self):
+        """Return student profile URL"""
         return reverse("scanner:profile", kwargs={"student_id": self.pk})
 
     def __str__(self):
-        return self.first_name.title() + " " + self.second_name.title()
+        return f"{self.first_name}".title() + " " + f"{self.second_name}".title()
 
 
-class List_Of_Emails(models.Model):
-    email = models.EmailField(null=True, blank=True)
-
-
-class Staff(models.Model):
-    """Database table for storing information
-    about a particular staff that borrowed a book"""
+class Staff(UserAbstractModel, models.Model):
+    """Database table for staff information"""
 
     class Meta:
-        ordering = ["-registration_date"]
-        verbose_name_plural = "staffs"
+        """Additional information for the Staff Class"""
 
-    image = models.ImageField(upload_to="images//%Y/%m/%d/", blank=False)
-    first_name = models.CharField(max_length=100)
-    second_name = models.CharField(max_length=100)
+        ordering = ["-registration_date"]
+        verbose_name_plural = "staff"
+
     staff_id = models.CharField(max_length=15, unique=True)
-    Email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=11, unique=True)
-    registration_date = models.DateTimeField(auto_now_add=True)
-    email_sent = models.BooleanField(null=True, blank=True, default=False)
-    sms_sent = models.BooleanField(null=True, blank=True, default=False)
 
     def get_absolute_url(self):
+        """Return staff profile URL"""
         return reverse("scanner:staffprofile", kwargs={"staff_id": self.pk})
 
     def __str__(self):
-        return self.first_name.title() + " " + self.second_name.title()
+        return f"{self.first_name}".title() + " " + f"{self.second_name}".title()
 
 
-class StudentBook(models.Model):
-    """create a book borrowed by a particular student"""
+class BookAbstractModel(models.Model):
+    """Abstract Class for StudentBook and StaffBook Classes"""
 
     class Meta:
-        ordering = ["-issued_date"]
+        """Additional information for the Class"""
+
+        abstract = True
 
     title = models.CharField("Book title", max_length=100)
-    borrowed_by = models.ForeignKey(
-        Student, on_delete=models.CASCADE, related_name="books", null=True, blank=True
-    )
     issued_date = models.DateTimeField(default=datetime.now())
-    # added_days = models.IntegerField(null=True, blank=True)
+    added_days = models.IntegerField(null=True, blank=True)
     remaining_days = models.CharField(max_length=100, null=True, blank=True)
     expiring_date = models.DateTimeField(
         default=(datetime.today() + timedelta(days=14)), null=True, blank=True
     )
 
+
+class StudentBook(BookAbstractModel, models.Model):
+    """Create a book borrowed by a Student"""
+
+    class Meta:
+        """Additional information for the Class"""
+
+        ordering = ["-issued_date"]
+
+    borrowed_by = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name="books", null=True, blank=True
+    )
+
     def get_absolute_url(self):
-        student_id = self.borrowed_by.id
+        """Return Student Profile URL"""
+        student_id = self.borrowed_by.pk
         return reverse("scanner:profile", kwargs={"student_id": student_id})
 
     def __str__(self):
         return self.title[:50]
 
 
-class StaffBook(models.Model):
-    """create a book borrowed by a particular staff"""
+class StaffBook(BookAbstractModel, models.Model):
+    """Create a book borrowed by a Staff"""
 
     class Meta:
+        """Additional information for the Class"""
         ordering = ["-issued_date"]
 
-    title = models.CharField("Book title", max_length=100)
     borrowed_by = models.ForeignKey(
         Staff, on_delete=models.CASCADE, related_name="books", null=True, blank=True
     )
-    issued_date = models.DateTimeField(default=datetime.now())
-    # added_days = models.IntegerField(null=True, blank=True)
-    remaining_days = models.CharField(max_length=100, null=True, blank=True)
-    expiring_date = models.DateTimeField(
-        default=(datetime.today() + timedelta(days=21)), null=True, blank=True
-    )
 
     def get_absolute_url(self):
+        """Return a Staff Profile URL"""
         staff_id = self.borrowed_by.pk
         return reverse("scanner:staffprofile", kwargs={"staff_id": staff_id})
 
@@ -139,18 +151,21 @@ class StaffBook(models.Model):
 
 
 @receiver(post_save, sender=Student, dispatch_uid="send_email_to_student")
-def send_email_to_student(sender, **kwargs):
+def send_email_to_student(**kwargs):
+    """Send Email to newly registered Student after successful registration"""
     student = kwargs["instance"]
     student_name = str(student.first_name) + " " + str(student.second_name)
     student_email = student.Email
     message = (
-        "Hello {}, thanks for registering with us! Attached is our rules"
-        " and regulations for you to read! thanks".format(student_name)
+        f"Dear {student_name}, thanks for registering with us! Attached is our rules"
+        " and regulations for you to read! Thank you so much."
     )
+    # Check if email not sent
     if not student.email_sent:
-        email = EmailMessage(
-            "Welcome Email from YSU YSU-Library_Team", message, [student_email]
-        )
+        # Resend the email
+        email_subject = "Registration Successful!"
+        email = EmailMessage(email_subject, message, [student_email])
+        # Attach the Library Rules and Regulations document to the Email
         email.attach_file("static/files/rules_and_regulations.pdf")
         email.send(fail_silently=False)
         student.email_sent = True
@@ -160,20 +175,21 @@ def send_email_to_student(sender, **kwargs):
 
 
 @receiver(post_save, sender=Staff, dispatch_uid="send_email_to_staff")
-def send_email_to_staff(sender, **kwargs):
+def send_email_to_staff(**kwargs):
+    """Send Email to newly registered Staff after sucessful registration"""
     staff = kwargs["instance"]
     staff_name = str(staff.first_name) + " " + str(staff.second_name)
     staff_email = staff.Email
     message = (
-        "Hello {}, thanks for registering with us!"
-        "Attached is our rules and regulations for you to read! thanks".format(
-            staff_name
-        )
+        f"Dear {staff_name}, thanks for registering with us! Attached is our rules"
+        " and regulations for you to read! Thank you so much."
     )
+    # Check if email not sent
     if not staff.email_sent:
-        email = EmailMessage(
-            "Welcome Email from YSU YSU-Library_Team", message, [staff_email]
-        )
+        # Resend the email
+        email_subject = "Registration Successful!"
+        email = EmailMessage(email_subject, message, [staff_email])
+        # Attach the Library Rules and Regulations document to the Email
         email.attach_file("static/files/rules_and_regulations.pdf")
         email.send(fail_silently=False)
         staff.email_sent = True
@@ -182,13 +198,16 @@ def send_email_to_staff(sender, **kwargs):
         staff.save()
 
 
-class Overdue_Charges(models.Model):
+class OverDueCharges(models.Model):
+    """Charges for late returning of Books"""
+
     overdue = models.IntegerField()
 
     class Meta:
-        verbose_name_plural = 'overdue charges'
+        """Additional information for the Class"""
+
+        verbose_name_plural = "overdue charges"
 
     def __str__(self):
         value = self.overdue
-        return 'The current overdue charges is ' +\
-        str(value) + ' naira per day'
+        return "The current overdue charges is " + str(value) + " naira per day"
